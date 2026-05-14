31a22333e287324efca4a20336b67f26f7e26fcb
krasovski20121991-create <krasovski20121991@gmail.com>
2026-05-14 15:53:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "АнестХелп.UA працює"}
+
+if name == "main":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
